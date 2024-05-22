cc3db1ca0311d8a373f9ba44e35216bf7807091c
MATUTINO <MATUTINO@DESKTOP-BOMS0H0>
2024-05-22 11:07:40 -0500

    acv0003 Ultima

---
+++ src/com/distribuida/entities/categoria.java
@@ -0,0 +1,28 @@
+package com.distribuida.entities;
+
+public class categoria {
+
+		private String idcategoria;
+		private String categoria;
+		public categoria(String idcategoria, String categoria) {
+			super();
+			this.idcategoria = idcategoria;
+			this.categoria = categoria;
+		}
+		public String getIdcategoria() {
+			return idcategoria;
+		}
+		public void setIdcategoria(String idcategoria) {
+			this.idcategoria = idcategoria;
+		}
+		public String getCategoria() {
+			return categoria;
+		}
+		public void setCategoria(String categoria) {
+			this.categoria = categoria;
+		}
+		@Override
+		public String toString() {
+			return "categoria [idcategoria=" + idcategoria + ", categoria=" + categoria + "]";
+		}
+}
